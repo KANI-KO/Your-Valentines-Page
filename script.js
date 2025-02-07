b007cbfa1ff730dc8ba44425a6cb37f978a184ae
HappyLizarb <lelandlee.gz@gmail.com>
2025-02-07 02:10:29 -0800

    Update script.js

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // --- No Button Click Functionality ---
     noButton.addEventListener("click", () => {
-        if (noClicks === 5) {  // On the 6th click
+        if (noClicks === 4) {  // On the 6th click
             questionText.innerText = "Seems like you're a bit hesitant... How about clicking 'Yes, please!' instead?";
             noButton.style.display = "none"; // Hide original No button
     
